bb5f88be104285dbdec14043fccaef01b494633b
samuel.simandl <samuelskolasimandl@gmail.com>
2023-11-09 12:58:12 +0100

    zacatek pole

---
+++ src/array/cviko.java
@@ -0,0 +1,24 @@
+package array;
+
+public class cviko {
+    public static void main(String[] args) {
+        int[] pole = {5, 32, 21, 15};
+        int[] pole2 = new int[10];
+
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println(pole[i]);
+        }
+
+        for (int i = 0; i < pole2.length; i++) {
+            pole2[i] = (int)(Math.random()*101);
+        }
+
+        for (int i = 0; i < pole2.length; i++) {
+            System.out.println(pole2[i]);
+        }
+    }
+
+    }
+
+    
+
