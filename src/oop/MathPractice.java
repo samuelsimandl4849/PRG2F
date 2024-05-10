361a20ac1473181d5eff3f3cbc9e9cfa6574592e
bohac <honza.boh00@gmail.com>
2024-05-10 12:21:44 +0200

    oop - static

--- src/oop/MathPractice.java
+++ src/oop/MathPractice.java
@@ -1,21 +1,24 @@
 package oop;
 
-
-
 public class MathPractice {
-    public static double addition(double a, double b){
+
+    public double addition(double a, double b){
         return a+b;
     }
-    public double multiplication(double a, double b){
+
+    public static double multiplication(double a, double b){
         return a*b;
     }
+
     public static void main(String[] args) {
-        double a = 2;
-        double b = 16;
-        System.out.println(MathPractice.addition(a,b));
+        double a = 4.5;
+        double b = 9.9;
+
+        System.out.println(MathPractice.multiplication(a,b));
 
         MathPractice practice = new MathPractice();
-        practice.multiplication(a,b);
-    }
 
+        System.out.println(practice.addition(a, b));
+
+    }
 }
